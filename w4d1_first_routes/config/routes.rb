4bb2e7801dc5f04373a7b6d62a6a09e41b8dfb51
joshhwang1125 <joshhwang1125@gmail.com>
2016-01-25 14:58:02 -0800

    create routes and controllers

---
+++ w4d1_first_routes/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  #resources :users
+  #USERS
+  get 'users' => 'users#index'
+  post 'users' => 'users#create'
+
+  #USER/ID
+
+  get 'users/:id' => 'users#show', :as => 'user'
+  patch 'users/:id' => 'users#update'
+  put 'users/:id' => 'users#update'
+  delete 'users/:id' => 'users#destroy'
+end
---
+++ w4d1_first_routes/db/migrate/20160125225723_create_contact_tables.rb
@@ -0,0 +1,6 @@
+class CreateContactTables < ActiveRecord::Migration
+  def change
+    create_table :contact_tables do |t|
+    end
+  end
+end
